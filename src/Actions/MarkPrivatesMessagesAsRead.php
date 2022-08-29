47effecdd12d2017d186e95a75889134cc95eecd
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-08-29 09:51:31 +0000

    One task within one method

--- src/Actions/MarkPrivatesMessagesAsRead.php
+++ src/Actions/MarkPrivatesMessagesAsRead.php
@@ -14,25 +14,15 @@ use Spatie\LaravelData\DataCollection;
 class MarkPrivatesMessagesAsRead implements MarkPrivatesMessagesAsReadContract
 {
     /**
-     * Mark given private messages as has been read.
+     * Notify the sender that the receiver has read private messages.
      *
-     * @param \Illuminate\Foundation\Auth\User&\Haemanthus\Basement\Contracts\User $receiver
      * @param \Spatie\LaravelData\DataCollection $privateMessages
-     * @return \Spatie\LaravelData\DataCollection
+     * @return void
      */
-    public function markAsRead(Authenticatable $receiver, DataCollection $privateMessages): DataCollection
+    protected function notifySenders(DataCollection $privateMessages): void
     {
-        Gate::forUser($receiver)->authorize('mark-as-read', $privateMessages);
-
         /** @var \Illuminate\Support\Collection<int,\Haemanthus\Basement\Data\PrivateMessageData> $collection */
         $collection = $privateMessages->toCollection();
-
-        Basement::newPrivateMessageModel()
-            ->whereIn('id', $collection->pluck('id'))
-            ->whereNull('read_at')
-            ->update([
-                'read_at' => now(),
-            ]);
 
         /** @var \Illuminate\Support\Collection<int,\Illuminate\Foundation\Auth\User&\Haemanthus\Basement\Contracts\User> $senders */
         $senders = $collection->pluck('sender')->unique();
@@ -44,6 +34,27 @@ class MarkPrivatesMessagesAsRead implements MarkPrivatesMessagesAsReadContract
                 privateMessages: $collection->filter(fn (PrivateMessageData $data): bool => $data->id === $sender->id),
             ),
         ));
+    }
+
+    /**
+     * Mark given private messages as has been read.
+     *
+     * @param \Illuminate\Foundation\Auth\User&\Haemanthus\Basement\Contracts\User $receiver
+     * @param \Spatie\LaravelData\DataCollection $privateMessages
+     * @return \Spatie\LaravelData\DataCollection
+     */
+    public function markAsRead(Authenticatable $receiver, DataCollection $privateMessages): DataCollection
+    {
+        Gate::forUser($receiver)->authorize('mark-as-read', $privateMessages);
+
+        Basement::newPrivateMessageModel()
+            ->whereIn('id', $privateMessages->toCollection()->pluck('id'))
+            ->whereNull('read_at')
+            ->update([
+                'read_at' => now(),
+            ]);
+
+        $this->notifySenders($privateMessages);
 
         return $privateMessages;
     }
