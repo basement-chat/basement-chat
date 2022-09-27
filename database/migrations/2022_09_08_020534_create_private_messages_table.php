ae9ef3d445f2230beddbf901874d5c932dd61a2b
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-09-27 23:14:26 +0700

    Fixed foreign key reference

--- database/migrations/2022_09_08_020534_create_private_messages_table.php
+++ database/migrations/2022_09_08_020534_create_private_messages_table.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Models\User;
 use BasementChat\Basement\Enums\MessageType;
-use BasementChat\Basement\Facades\Basement;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,15 +14,16 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::create('private_messages', static function (Blueprint $table): void {
-            Basement::useUserModel(config(key: 'basement.user_model', default: User::class));
-
-            $user = Basement::newUserModel();
+            /** @var \Illuminate\Database\Eloquent\Model $user */
+            $user = app(config('basement.user_model'));
+            $primaryKey = $user->getKeyName();
+            $tableName = $user->getTable();
 
             $table->id();
             $table->unsignedBigInteger('receiver_id');
             $table->unsignedBigInteger('sender_id');
-            $table->foreign('receiver_id')->references('id')->on($user->getTable());
-            $table->foreign('sender_id')->references('id')->on($user->getTable());
+            $table->foreign('receiver_id')->references($primaryKey)->on($tableName);
+            $table->foreign('sender_id')->references($primaryKey)->on($tableName);
             $table->enum('type', [MessageType::document()->value, MessageType::text()->value]);
             $table->string('value');
             $table->timestamp('read_at')->nullable();
