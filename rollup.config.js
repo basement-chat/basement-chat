6235c4050f82c41ba32fe7ad3152bc49fbb256ad
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-10-07 18:35:54 +0700

    Add slim, cjs & esm bundle

--- rollup.config.js
+++ rollup.config.js
@@ -9,48 +9,84 @@ import postcss from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 
-export default defineConfig({
-  input: './resources/js/app.ts',
-  output: {
-    sourcemap: true,
-    name: 'Basement Bundle',
-    file: './resources/dist/basement.bundle.min.js',
-    format: 'umd',
-  },
-  plugins: [
-    resolve({
-      browser: true,
-    }),
-    commonjs(),
-    json(),
-    nodePolyfills(),
-    postcss({
-      extract: true,
-      minimize: true,
-    }),
-    terser(),
-    typescript({
-      tsconfigOverride: {
-        compilerOptions: {
-          declaration: false,
+const plugins = {
+  resolve: resolve({
+    browser: true,
+  }),
+  commonjs: commonjs(),
+  json: json(),
+  nodePolyfills: nodePolyfills(),
+  postcss: postcss({
+    extract: true,
+    minimize: true,
+  }),
+  terser: terser(),
+  typescript: typescript(),
+  babel: babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    extensions: ['.ts'],
+    include: ['./resources/js/**/*.ts'],
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '3.25',
         },
-      },
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      extensions: ['.ts'],
-      include: ['./resources/js/**/*.ts'],
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            useBuiltIns: 'usage',
-            corejs: '3.25',
-          },
-        ],
       ],
-    }),
-    filesize(),
-  ],
-})
+    ],
+  }),
+  filesize: filesize(),
+}
+
+export default defineConfig([
+  {
+    input: './resources/js/app.bundle.ts',
+    output: {
+      sourcemap: true,
+      name: 'Basement Bundle',
+      file: './dist/basement.bundle.min.js',
+      format: 'umd',
+    },
+    plugins: Object.values(plugins),
+  },
+  {
+    input: './resources/js/app.slim.ts',
+    output: {
+      sourcemap: true,
+      name: 'Basement Slim',
+      file: './dist/basement.slim.min.js',
+      format: 'umd',
+    },
+    plugins: [
+      plugins.resolve,
+      plugins.commonjs,
+      plugins.json,
+      plugins.nodePolyfills,
+      plugins.terser,
+      plugins.typescript,
+      plugins.babel,
+      plugins.filesize,
+    ],
+  },
+  {
+    input: './resources/js/plugins/basement.ts',
+    output: [
+      { file: './dist/basement.plugin.esm.js', format: 'esm' },
+      { file: './dist/basement.plugin.common.js', format: 'cjs', exports: 'default' },
+    ],
+    plugins: [
+      resolve(),
+      plugins.commonjs,
+      typescript({
+        exclude: [
+          './resources/js/*.ts',
+          './resources/js/@types/*.ts',
+        ],
+      }),
+      plugins.babel,
+      plugins.filesize,
+    ],
+  },
+])
