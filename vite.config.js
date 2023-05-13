702279655db6b60968c6c0641cd67b0e4cf242ce
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-05-13 16:50:30 +0700

    chore(vite): ignore vendor folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: [
+        '**/vendor/**',
+      ],
+    },
+  },
   plugins: [
     laravel({
       input: [
